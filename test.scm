fabbddc61120bbc001ca268a7e349976d61949b3
andres santana <andres.santana@gmail.com>
2010-12-29 09:21:49 -0400

    added gitignore list and fib test procedure

---
+++ test.scm
@@ -0,0 +1,8 @@
+;; A procedure for counting the n:th Fibonacci number
+;; See SICP, p. 37
+(define fib
+  (lambda (n)
+    (cond ((= n 0) 0)  
+          ((= n 1) 1)
+          (else (+ (fib (- n 1))
+                   (fib (- n 2)))))))
